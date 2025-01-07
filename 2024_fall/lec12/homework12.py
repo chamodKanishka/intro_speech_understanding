57df3a73e3196f9960e3baddcd523c281880c7cc
Chamod Kanishka <chamodkanishka77@gmail.com>
2025-01-07 10:32:08 +0900

    hw 12
    completed homework 12

--- 2024_fall/lec12/homework12.py
+++ 2024_fall/lec12/homework12.py
@@ -1,13 +1,14 @@
 import gtts
 
 def synthesize(text, lang, filename):
-    '''
-    Use gtts.gTTs(text=text, lang=lang) to synthesize speech, then write it to filename.
+    # gtts.gTTS(text=text, lang=lang)
     
-    @params:
-    text (str) - the text you want to synthesize
-    lang (str) - the language in which you want to synthesize it
-    filename (str) - the filename in which it should be saved
-    '''
-    raise RuntimeError("FAIL! You need to change this function so that it works!")
+    # @params:
+    # text (str) - the text you want to synthesize
+    # lang (str) - the language in which you want to synthesize it
+    # filename (str) - the filename in which it should be saved
+    
+    # raise RuntimeError("FAIL! You need to change this function so that it works!")
+    
+    gtts.gTTS(text=text,lang=lang).save(filename)
 
